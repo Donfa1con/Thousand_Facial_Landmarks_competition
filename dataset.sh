0d3dd0752a15e9356bbb7f8ac58e10d8f8f5d405
Dmitry Ziganshin <diziganshin@inbox.ru>
2020-05-13 16:54:06 +0300

    Fix train configs and add download link for dataset

--- dataset.sh
+++ dataset.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# https://drive.google.com/open?id=1fD9BlL9VEc0WC3ybRWc1LRZKfYHftR4G dataset link
+
 fileid="1fD9BlL9VEc0WC3ybRWc1LRZKfYHftR4G"
 filename="dataset.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"
